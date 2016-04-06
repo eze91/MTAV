474053d23179430f61a9a19592558b6fe4d31213
Agustina Sanchez <eze91@outlook.com>
2016-04-06 18:17:24 -0300

    andando

--- MTAV_asign.sol
+++ MTAV_asign.sol
@@ -1,7 +1,7 @@
 Problem:    MTAV_empate
-Rows:       18
-Columns:    10 (10 integer, 9 binary)
-Non-zeros:  67
+Rows:       23
+Columns:    17 (17 integer, 16 binary)
+Non-zeros:  117
 Status:     INTEGER OPTIMAL
 Objective:  resultado = 2 (MINimum)
 
@@ -14,45 +14,62 @@ Objective:  resultado = 2 (MINimum)
                                    1            -0               
      4 z_menorIgual[c2]
                                    0            -0               
-     5 unicaAsignacionCoperativista_mayorIgual[c0]
+     5 z_menorIgual[c3]
+                                   1            -0               
+     6 unicaAsignacionCoperativista_mayorIgual[c0]
                                    1             1               
-     6 unicaAsignacionCoperativista_mayorIgual[c1]
+     7 unicaAsignacionCoperativista_mayorIgual[c1]
                                    1             1               
-     7 unicaAsignacionCoperativista_mayorIgual[c2]
+     8 unicaAsignacionCoperativista_mayorIgual[c2]
                                    1             1               
-     8 unicaAsignacionCoperativista_menorIgual[c0]
-                                   1                           1 
-     9 unicaAsignacionCoperativista_menorIgual[c1]
-                                   1                           1 
-    10 unicaAsignacionCoperativista_menorIgual[c2]
-                                   1                           1 
-    11 unicaAsignacionCasa_mayorIgual[v0]
+     9 unicaAsignacionCoperativista_mayorIgual[c3]
                                    1             1               
-    12 unicaAsignacionCasa_mayorIgual[v1]
+    10 unicaAsignacionCoperativista_menorIgual[c0]
+                                   1                           1 
+    11 unicaAsignacionCoperativista_menorIgual[c1]
+                                   1                           1 
+    12 unicaAsignacionCoperativista_menorIgual[c2]
+                                   1                           1 
+    13 unicaAsignacionCoperativista_menorIgual[c3]
+                                   1                           1 
+    14 unicaAsignacionCasa_mayorIgual[v0]
                                    1             1               
-    13 unicaAsignacionCasa_mayorIgual[v2]
+    15 unicaAsignacionCasa_mayorIgual[v1]
                                    1             1               
-    14 unicaAsignacionCasa_menorIgual[v0]
+    16 unicaAsignacionCasa_mayorIgual[v2]
+                                   1             1               
+    17 unicaAsignacionCasa_mayorIgual[v3]
+                                   1             1               
+    18 unicaAsignacionCasa_menorIgual[v0]
                                    1                           1 
-    15 unicaAsignacionCasa_menorIgual[v1]
+    19 unicaAsignacionCasa_menorIgual[v1]
                                    1                           1 
-    16 unicaAsignacionCasa_menorIgual[v2]
+    20 unicaAsignacionCasa_menorIgual[v2]
                                    1                           1 
-    17 s_mayorIgual                5             5               
-    18 s_menorIgual                5                           5 
+    21 unicaAsignacionCasa_menorIgual[v3]
+                                   1                           1 
+    22 s_mayorIgual                6             6               
+    23 s_menorIgual                6                           6 
 
    No. Column name       Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
      1 x[c0,v0]     *              0             0             1 
      2 x[c0,v1]     *              1             0             1 
      3 x[c0,v2]     *              0             0             1 
-     4 x[c1,v0]     *              1             0             1 
-     5 x[c1,v1]     *              0             0             1 
-     6 x[c1,v2]     *              0             0             1 
-     7 x[c2,v0]     *              0             0             1 
-     8 x[c2,v1]     *              0             0             1 
-     9 x[c2,v2]     *              1             0             1 
-    10 z            *              2                             
+     4 x[c0,v3]     *              0             0             1 
+     5 x[c1,v0]     *              1             0             1 
+     6 x[c1,v1]     *              0             0             1 
+     7 x[c1,v2]     *              0             0             1 
+     8 x[c1,v3]     *              0             0             1 
+     9 x[c2,v0]     *              0             0             1 
+    10 x[c2,v1]     *              0             0             1 
+    11 x[c2,v2]     *              1             0             1 
+    12 x[c2,v3]     *              0             0             1 
+    13 x[c3,v0]     *              0             0             1 
+    14 x[c3,v1]     *              0             0             1 
+    15 x[c3,v2]     *              0             0             1 
+    16 x[c3,v3]     *              1             0             1 
+    17 z            *              2                             
 
 Integer feasibility conditions:
 
